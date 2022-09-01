6c684424d66a1ca49d55bba4fe2cedd999664d64
Kummirai <ajaxmilton@hotmail.com>
2022-09-01 19:03:44 +0200

    Sum of unique list

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    #insert list to a set
-    unique_int = set(my_list)
-
-    #Convert the set to a list
-    unique_list = list(unique_int)
+    unique_list = []
+    unique_list = list(set(my_list))
     total = 0
-    #transverse through the list
-    for i in range(0, len(my_list)):
-        total = total + my_list[i]
-        print(i)
-        return(total)
+    total = sum(unique_list)
+    return(total)
