ae7349b98fca039c75bd8c1aabe390dd1ca395ed
kummirai <ajaxmilton@hotmail.com>
2022-09-18 16:54:36 +0200

    Fix code

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-class Square:
-    """An empty class Square."""
+'''Square class definition'''
+class Square():
     pass
-
-if __name__ == '__main__':
-    my_square = Square()
-    print(type(my_square))
-    print(my_square.__dict__)
