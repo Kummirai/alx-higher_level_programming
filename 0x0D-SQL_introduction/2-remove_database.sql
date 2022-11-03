49bb7b7181f31153315997bbde9bfe03e38643ab
Kummirai <ajaxmilton@hotmail.com>
2022-11-03 10:06:56 +0200

    Fix typo

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,3 +1,3 @@
 -- Script that deletes the database hbtn_0c_0
 -- If the database hbtn_0c_0 doesn’t exist, your script should not fail
-DROP DATABASE IF EXITS hbtn_0c_0;
+DROP DATABASE IF EXISTS hbtn_0c_0;
