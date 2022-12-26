d60792381e1001b4858b9f830f98aa699e1fabf5
Kummirai <ajaxmilton@hotmail.com>
2022-12-25 23:44:26 -0800

    Fix typo

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+#Script that lists all the states fron the database htbn_0e_0_usa
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
+    ''' connects to the database '''
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+
+    ''' cursor for multiple separate working envs thru one connection '''
+    cur = db.cursor()
+
+    ''' execute SQL queries '''
+    cur.execute("SELECT * FROM states ORDER BY id")
+
+    ''' fetch all-at-once '''
+    rows = cur.fetchall()
+    for col in rows:
+        print("{}".format(col))
+    cur.close()
+    db.close()
