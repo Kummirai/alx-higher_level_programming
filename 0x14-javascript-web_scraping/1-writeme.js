0123ac5d89a90ae325ee91a66c26d07527acd701
Kummirai <ajaxmilton@hotmail.com>
2023-08-09 23:35:19 +0200

    Fix code

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,19 +1,8 @@
 #!/usr/bin/node
-
-const fs = require('fs');
-
-function writeFile(filePath, content) {
-  fs.writeFile(filePath, content, 'utf-8', (err) => {
-    if (err) {
-      console.error(err);
-    } else {
-      console.log(`Content written to ${filePath}`);
-    }
-  });
-}
-
-const filePath = process.argv[2];
-const content = process.argv[3];
-
-writeFile(filePath, content);
-
+// Script that writes a string to a file
+let fs = require('fs');
+fs.writeFile(process.argv[2], process.argv[3], 'utf8', function (err) {
+  if (err) {
+    return console.log(err);
+  }
+});
