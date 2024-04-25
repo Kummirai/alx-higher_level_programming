2c0f5e3f55aaddc29718f5a0c27ddd48b46f7f0b
Milton <104756300+Kummirai@users.noreply.github.com>
2024-04-26 01:40:15 +0300

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 # Sends a request to the passed in URL and displays size of the body response
-curl -sI "$1" | grep "Content-Length" | cut -d' ' -f2
+
+URL=$1
+
+IFS=$'\n' read -d "" body code  < <(curl -s -w "\n%{http_code}\n" "$URL")
+
+echo "HTTP Code: $code"
+echo "Response Body Size: ${#body} bytes"
