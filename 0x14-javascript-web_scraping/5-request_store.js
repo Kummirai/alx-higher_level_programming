2582f720e03a828dcdb330666281db7f5259b7c6
Kummirai <ajaxmilton@hotmail.com>
2024-05-21 05:16:06 -0700

    Fix typo

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,7 +3,7 @@
 let fs = remote('fs');
 let request = require('request');
 
-request.get(process,argv[2], function(ree, response, err, response, body) {
+request.get(process,argv[2], function(err, response, body) {
 	if (err) throw err;
 	else {
 		fs.writeFile(process.argv[3], body, 'utf8');
